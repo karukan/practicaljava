709c992b3c5ac079f97aedffdfbfe5c05995463a
karukan <75187667+karukan@users.noreply.github.com>
2025-01-05 11:53:56 +0800

    first commit
    added log.info of sl4j

--- src/main/java/com/course/practicaljava/api/server/DefaultRestAPI.java
+++ src/main/java/com/course/practicaljava/api/server/DefaultRestAPI.java
@@ -2,6 +2,8 @@ package com.course.practicaljava.api.server;
 
 import java.time.LocalTime;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,8 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping( "/api")
 public class DefaultRestAPI {
 
+	private static final  Logger LOG = LoggerFactory.getLogger(DefaultRestAPI.class);
+	
 	@GetMapping("/welcome")
 	public String welcome() {
+		LOG.info( "Swamiye Saranam Iyyappa");
 		return "Swamiye Saranam Ayyappa";
 	}
 	
