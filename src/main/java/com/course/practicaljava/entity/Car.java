2dd9858a991cdbeecd73c1cfb53420fdce1fcbe3
karukan <75187667+karukan@users.noreply.github.com>
2025-01-07 15:11:23 +0800

    add int, boolean, date

---
+++ src/main/java/com/course/practicaljava/entity/Car.java
@@ -0,0 +1,63 @@
+package com.course.practicaljava.entity;
+
+import java.util.Date;
+
+public class Car {
+	private String brand;
+	private String color;
+	private String type;
+	private int price;
+	private boolean available;
+	private Date firstReleaseDate;
+	
+	public Car() {
+		
+	}
+	public Car(String brand, String color, String type) {
+		super();
+		this.brand = brand;
+		this.color = color;
+		this.type = type;
+	}
+	public String getBrand() {
+		return brand;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public boolean isAvailable() {
+		return available;
+	}
+	public void setAvailable(boolean available) {
+		this.available = available;
+	}
+	public Date getFirstReleaseDate() {
+		return firstReleaseDate;
+	}
+	public void setFirstReleaseDate(Date firstReleaseDate) {
+		this.firstReleaseDate = firstReleaseDate;
+	}
+	public String getColor() {
+		return color;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	@Override
+	public String toString() {
+		return "Car [brand=" + brand + ", color=" + color + ", type=" + type + ", price=" + price + ", available="
+				+ available + ", firstReleaseDate=" + firstReleaseDate + "]";
+	}
+}
---
+++ src/main/java/com/course/practicaljava/service/CarService.java
@@ -0,0 +1,14 @@
+package com.course.practicaljava.service;
+
+import java.util.List;
+
+import com.course.practicaljava.entity.Car;
+
+public interface CarService {
+	List<String> BRANDS = List.of("Toyota", "Honda", "Ford");
+	List<String> COLORS = List.of("Red", "B lack", "White");
+	List<String> TYPES = List.of("Sedan", "SUV", "MPV");
+
+	Car generateCar();
+
+}
